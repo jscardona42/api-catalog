e57d338551aef6f3982093f565891c56f44b919b
jscardona42 <35873185+jscardona42@users.noreply.github.com>
2022-01-28 16:30:42 -0500

    Instalación de paquetes
    Instalación de Mailer para envío de correo de bienvenida.
    Instalación de jwt y passport para autenticación y authorización.
    Instalación de bcrypt para cifrado de contraseña.
    Instalación de class-validator y class-transformer.
    Instalación de prisma.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,19 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { SuppliersModule } from './suppliers/suppliers.module';
 import { CatalogsModule } from './catalogs/catalogs.module';
+import { PrismaService } from './prisma.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, SuppliersModule, CatalogsModule],
+  imports: [
+    UsersModule,
+    SuppliersModule,
+    CatalogsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService]
 })
-export class AppModule {}
+export class AppModule { }
