ae3bcb58f9867bdeec3d343f8355c1ddea633018
jscardona42 <35873185+jscardona42@users.noreply.github.com>
2022-01-28 17:09:36 -0500

    Se crea guard
    Se crea guard para valdiar el token del usuario y autorizar la consulta de productos.

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,42 @@
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import * as jwt from "jsonwebtoken";
+import { PrismaService } from '../prisma.service';
+
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+
+    async canActivate(context: ExecutionContext) {
+
+        const request = context.switchToHttp().getRequest();
+        let token: any;
+
+        if (request.headers.authorization !== undefined) {
+            const authorization = request.headers.authorization;
+            try {
+                token = authorization.split(" ")[1]
+                jwt.verify(token, process.env.JWT_SECRET);
+            } catch (error) {
+                throw new UnauthorizedException(error);
+            }
+        } else {
+            throw new UnauthorizedException("Unauthorized user");
+        }
+        if (request.body.user_id !== undefined) {
+            await this.validateToken(request.body.user_id, token);
+        }
+        return true;
+    }
+
+    async validateToken(user_id: number, token: string) {
+        let prismaService = new PrismaService();
+        let user = await prismaService.users.findUnique({
+            where: { user_id }
+        });
+
+        if (user.token !== token) {
+            throw new UnauthorizedException("The token does not belong to the user");
+        }
+    }
+}
